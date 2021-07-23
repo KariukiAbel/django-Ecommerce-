bb6fc8dbb3bcfc9d80ca6075db89ed2e9dabee53
KariukiAbel <abelmuriithi@gmail.com>
2021-07-23 14:57:37 +0300

    Made all necessary app installations,made sure all the urls are pointing to the right places and the product model is done

--- EcommerceSystem/EcommerceSystem/urls.py
+++ EcommerceSystem/EcommerceSystem/urls.py
@@ -18,4 +18,6 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path(r'accounts/', include('accounts.urls')),
+    path(r'products/', include('products.urls')),
 ]
