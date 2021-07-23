d497123cd5a7d865a345013cef286caf6b8589b9
KariukiAbel <abelmuriithi@gmail.com>
2021-07-23 17:25:43 +0300

    I don't know what I am doing but I'm optimistic I'll learn and understand more django and do more projects in the near future

---
+++ EcommerceSystem/accounts/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import url
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path(r'signup/', views.signup_view, name = 'signup'),
+    path(r'login/', views.login_view, name = 'login'),
+]
