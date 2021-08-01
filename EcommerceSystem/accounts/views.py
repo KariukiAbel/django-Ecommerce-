19e4d595aa4419f49b35b57e028d2e78fb87ead2
KariukiAbel <abelmuriithi@gmail.com>
2021-08-01 23:27:40 +0300

    Learned a new thing which is getting user data from html form to django views using request.POST.get() funtion. Also had my friend over, David Kinyajui who helped me setup bootstra

--- EcommerceSystem/accounts/views.py
+++ EcommerceSystem/accounts/views.py
@@ -9,8 +9,13 @@ def signup_view(request):
 
 def login_view(request):
     if request.method == "POST":
+        user_mail = request.POST.get('uname')
+        password_entered = request.POST.get('password')
+        print(user_mail)
+        print(password_entered)
         return render(request, "products/products.html")
     return render(request, 'accounts/login.html')
 
 def reset_password(request):
+    users = Users.objects.all();
     return render(request, 'accounts/resetpassword.html')
