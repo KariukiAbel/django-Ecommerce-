9278c3f71ad1678a56254ebc758b5b8a9c5e4cd3
KariukiAbel <abelmuriithi@gmail.com>
2021-08-18 18:38:29 +0300

    Added the checkout page and I've made some steps on the cart app

--- EcommerceSystem/cart/urls.py
+++ EcommerceSystem/cart/urls.py
@@ -4,5 +4,6 @@ from cart import views
 
 urlpatterns = [
     path('', views.cart_view, name='cart'),
+    path('checkout/', views.checkout_view, name='checkout')
   
 ]
--- EcommerceSystem/cart/views.py
+++ EcommerceSystem/cart/views.py
@@ -3,4 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 
 def cart_view(request):
-    pass
+    return render(request, 'cart/index.html')
+
+def checkout_view(request):
+    return render(request, 'cart/checkout.html')
