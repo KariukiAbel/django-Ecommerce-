5efa115dc1d8f898245f411d682067fd9b02e3e7
KariukiAbel <abelmuriithi@gmail.com>
2021-08-19 21:25:55 +0300

    Broke down the Product model to create another model that will house the product description and also linked both tables

--- EcommerceSystem/products/models.py
+++ EcommerceSystem/products/models.py
@@ -4,14 +4,24 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Product(models.Model):
     name = models.CharField(max_length=100)
-    description = models.TextField(max_length=255)
-    image = models.ImageField(default='default.png', blank=True)
     date_added = models.DateField(auto_now_add = True)
     price = models.FloatField(max_length=100)
-    category = models.CharField(max_length=100)
     added_by = models.ForeignKey(User, on_delete=models.CASCADE)
     in_store = models.BooleanField(default="True")
     
     
     def __str__(self):
-        return f"{self.name}--{self.description}"
+        return f"{self.name}"
+    
+    
+class Product_description(models.Model):
+    image = models.ImageField()
+    category = models.CharField(max_length=100)
+    unit_description = models.CharField(max_length= 100)
+    description = models.TextField(max_length=255)
+    quantity = models.IntegerField()
+    product_id = models.OneToOneField(Product, on_delete=models.CASCADE)
+    
+    
+    def __str__(self):
+        return f'{self.description}'
